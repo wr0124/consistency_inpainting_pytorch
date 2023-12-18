1ac05a64f3f83fb99cc499679b6fc2379def970e
julie <wr0124@gmail.com>
2023-12-18 15:36:35 +0100

    feat: .sh modify

--- run_model.sh
+++ run_model.sh
@@ -1,14 +1,14 @@
 python3 train.py \
---data_dir "/data3/juliew/datasets/anime_faces" \
---image_size 32 32 \
---batch_size 32 \
+--data_dir "/data3/juliew/datasets/noglasses2glasses_ffhq/trainB" \
+--image_size 32 32  \
+--batch_size 32  \
 --num_workers 12 \
 --max_steps 100000 \
---sample_every_n_steps  10 \
+--sample_every_n_epochs 1 \
 --devices 1 \
 --lr  1e-4 \
 --lr_scheduler_start_factor 1e-5 \
 --lr_scheduler_iters 10_000 \
 --num_samples 32 \
---env "consistency_model_anime_face" \
+--env "consistency_model_cat_pytorch" \
 --train_continue
